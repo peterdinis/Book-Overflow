a7696e276fa262391e9bb4f934d9ac181b16d04c
peterdinis <petter1@pobox.sk>
2025-10-19 11:44:24 +0200

    infra: Adding typesense and start working on search service WIP

--- BookOverflow.AppHost/AppHost.cs
+++ BookOverflow.AppHost/AppHost.cs
@@ -5,6 +5,14 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
 
 //TODO For now sqlite later postgresql
 
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
+    .WithArgs("--data-dir", "/data", "--api-key", "xyz", "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithEndpoint(8108, 8108, name: "typesense");
+
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
+
 var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
     .WithReference(keycloak)
     .WaitFor(keycloak);
