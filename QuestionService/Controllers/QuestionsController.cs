78795234a52fe096baed18b8c8d5ada591d506bc
peterdinis <petter1@pobox.sk>
2025-10-20 15:11:44 +0200

    feat: Working on search service

--- QuestionService/Controllers/QuestionsController.cs
+++ QuestionService/Controllers/QuestionsController.cs
@@ -1,16 +1,18 @@
 using System.Security.Claims;
+using Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuestionService.Data;
 using QuestionService.DTOs;
 using QuestionService.Models;
-
+using Wolverine;
+    
 namespace QuestionService.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class QuestionsController(QuestionDbContext db) : ControllerBase
+public class QuestionsController(QuestionDbContext db, IMessageBus bus) : ControllerBase
 {
     [Authorize]
     [HttpPost]
@@ -38,6 +40,8 @@ public class QuestionsController(QuestionDbContext db) : ControllerBase
         
         db.Questions.Add(question);
         await db.SaveChangesAsync();
+
+        await bus.PublishAsync(new QuestionCreated(question.Id, question.Title, question.Content, question.CreatedAt, question.TagSlugs));
         
         return Created($"/questions/{question.Id}", question);
     }
